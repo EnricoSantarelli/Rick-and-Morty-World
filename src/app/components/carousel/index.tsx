8c28f78842cdec37578816ba9f9d33ae2fbdd77a
EnricoSantarelli <enricosantarelli@hotmail.com>
2023-04-14 15:36:05 -0300

    added new package 🏎

--- src/app/components/carousel/index.tsx
+++ src/app/components/carousel/index.tsx
@@ -7,7 +7,25 @@ export default function Carousel() {
   const { characters } = useContext(CharacterContext);
 
   return (
-    <CarouselComponent cols={5} rows={2} gap={10} loop autoplay={3000}>
+    <CarouselComponent
+      cols={5}
+      rows={2}
+      gap={10}
+      loop
+      autoplay={3000}
+      responsiveLayout={[
+        {
+          breakpoint: 769,
+          cols: 2,
+          rows: 3,
+        },
+        {
+          breakpoint: 427,
+          cols: 2,
+          rows: 3,
+        },
+      ]}
+    >
       {characters.map((character) => (
         <CarouselComponent.Item key={character.id}>
           <div className={styles.carousel_card}>
