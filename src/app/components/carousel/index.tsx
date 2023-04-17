de31f237d6d35422363f0550b941fe7990925c93
EnricoSantarelli <enricosantarelli@hotmail.com>
2023-04-16 22:12:18 -0300

    changing swiper package 🏰

--- src/app/components/carousel/index.tsx
+++ src/app/components/carousel/index.tsx
@@ -1,38 +1,22 @@
 import React, { useContext } from "react";
 import { CharacterContext } from "@/contexts/character_provider";
 import styles from "./carousel.module.scss";
-import CarouselComponent from "better-react-carousel";
+import { Navigation, Pagination } from "swiper";
+import { Swiper } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default function Carousel() {
   const { characters } = useContext(CharacterContext);
 
   return (
-    <CarouselComponent
-      cols={5}
-      rows={2}
-      gap={10}
-      loop
-      autoplay={3000}
-      responsiveLayout={[
-        {
-          breakpoint: 769,
-          cols: 2,
-          rows: 3,
-        },
-        {
-          breakpoint: 427,
-          cols: 2,
-          rows: 3,
-        },
-      ]}
-    >
+    <Swiper>
       {characters.map((character) => (
-        <CarouselComponent.Item key={character.id}>
-          <div className={styles.carousel_card}>
-            <h1>{character.name}</h1>
-          </div>
-        </CarouselComponent.Item>
+        <div key={character.id} className={styles.carousel_card}>
+          <h1>{character.name}</h1>
+        </div>
       ))}
-    </CarouselComponent>
+    </Swiper>
   );
 }
