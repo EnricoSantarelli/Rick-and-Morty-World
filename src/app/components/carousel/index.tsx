cf3d85f74be24ca74c847c4e7dcf8683fdda864d
EnricoSantarelli <enricosantarelli@hotmail.com>
2023-04-18 09:26:49 -0300

    finishing carousel 🏖

--- src/app/components/carousel/index.tsx
+++ src/app/components/carousel/index.tsx
@@ -2,89 +2,97 @@ import React, { useContext } from "react";
 import { CharacterContext } from "@/contexts/character_provider";
 import styles from "./carousel.module.scss";
 import Image from "next/image";
-import { Navigation, Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Carousel() {
   const { characters } = useContext(CharacterContext);
 
+  var settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <Swiper
-      className={styles.carousel}
-      modules={[Navigation, Pagination]}
-      spaceBetween={50}
-      slidesPerView={2}
-      navigation
-      loop
-      centeredSlides={true}
-      breakpoints={{
-        769: {
-          slidesPerView: 2,
-        },
-        1025: {
-          slidesPerView: 3,
-        },
-        1440: {
-          slidesPerView: 4,
-        },
-      }}
-    >
+    <Slider {...settings} className={styles.carousel}>
       {characters.map((character) => (
-        <SwiperSlide key={character.id} className={styles.carousel__card}>
-          <Image
-            src={character.image}
-            alt="${character.name} image"
-            width={150}
-            height={150}
-            className={styles.carousel__image}
-            priority
-          />
-          <div className={styles.carousel__attributes}>
-            <h1 className={styles.carousel__name}>{character.name}</h1>
-            <div className={styles.carousel__div}>
-              <div className={styles.carousel__gender}>
-                <h2 className={styles.carousel__title}>Gender</h2>
+        <div key={character.id}>
+          <div className={styles.carousel__card}>
+            <Image
+              src={character.image}
+              alt="${character.name} image"
+              width={150}
+              height={150}
+              className={styles.carousel__image}
+              priority
+            />
+            <div className={styles.carousel__attributes}>
+              <h1 className={styles.carousel__name}>{character.name}</h1>
+              <div className={styles.carousel__div}>
+                <div className={styles.carousel__gender}>
+                  <h2 className={styles.carousel__title}>Gender</h2>
+                  <span className={styles.carousel__span}>
+                    {character.gender[0].toUpperCase() +
+                      character.gender.substring(1)}
+                  </span>
+                </div>
+                <div className={styles.carousel__species}>
+                  <h2 className={styles.carousel__title}>Specie</h2>
+                  <span className={styles.carousel__span}>
+                    {character.species[0].toUpperCase() +
+                      character.species.substring(1)}
+                  </span>
+                </div>
+              </div>
+              <div className={styles.carousel__origin}>
+                <h2 className={styles.carousel__title}>Origin</h2>
                 <span className={styles.carousel__span}>
-                  {character.gender[0].toUpperCase() +
-                    character.gender.substring(1)}
+                  {character.origin.name[0].toUpperCase() +
+                    character.origin.name.substring(1)}
                 </span>
               </div>
-              <div className={styles.carousel__species}>
-                <h2 className={styles.carousel__title}>Specie</h2>
-                <span className={styles.carousel__span}>
-                  {character.species[0].toUpperCase() +
-                    character.species.substring(1)}
-                </span>
+              <div className={styles.carousel__status}>
+                <div
+                  className={
+                    character.status === "Alive"
+                      ? styles.carousel__alive
+                      : character.status === "Dead"
+                      ? styles.carousel__dead
+                      : styles.carousel__unknown
+                  }
+                />
+                <h2 className={styles.carousel__span}>
+                  {character.status[0].toUpperCase() +
+                    character.status.substring(1)}
+                </h2>
               </div>
-            </div>
-            <div className={styles.carousel__origin}>
-              <h2 className={styles.carousel__title}>Origin</h2>
-              <span className={styles.carousel__span}>
-                {character.origin.name[0].toUpperCase() +
-                  character.origin.name.substring(1)}
-              </span>
-            </div>
-            <div className={styles.carousel__status}>
-              <div
-                className={
-                  character.status === "Alive"
-                    ? styles.carousel__alive
-                    : character.status === "Dead"
-                    ? styles.carousel__dead
-                    : styles.carousel__unknown
-                }
-              />
-              <h2 className={styles.carousel__span}>
-                {character.status[0].toUpperCase() +
-                  character.status.substring(1)}
-              </h2>
             </div>
           </div>
-        </SwiperSlide>
+        </div>
       ))}
-    </Swiper>
+    </Slider>
   );
 }
