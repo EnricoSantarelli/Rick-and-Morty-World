da0821fe0b7c4a0689eecdaf430e0e11130ddb0a
EnricoSantarelli <63019527+EnricoSantarelli@users.noreply.github.com>
2023-05-02 18:29:53 -0300

    testing other ways 👨

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logo from "../../assets/logo_rick_and_morty_world.svg";
+import linkedinIcon from "@/app/assets/logo_linkedin.svg";
 import styles from "./header.module.scss";
 import Link from "next/link";
 
@@ -15,6 +16,22 @@ export default function Header() {
           className={styles.header__logo}
           priority
         />
+        <Image
+          src={logo}
+          alt="Rick and Morty World Logo"
+          width={10}
+          height={10}
+          className={styles.header__logo}
+          priority
+        />
+        <Image
+              src={linkedinIcon}
+              alt="Linkedin logo"
+              width={10}
+              height={10}
+              className={styles.home__icon}
+              priority
+            />
       </Link>
       <div className={styles.header__div}>
         <Link className={styles.header__link} href="/create_character">
