e99f8c0051c90dfa26db99b5aa7e4e7faeadacb5
EnricoSantarelli <63019527+EnricoSantarelli@users.noreply.github.com>
2023-05-09 09:19:03 -0300

    creating base card 🎃

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -8,7 +8,30 @@ export default function CreatePage() {
       <div className={styles.create__header}>
         <h1 className={styles.create__title}>Create</h1>
       </div>
-      <div className={styles.create__card}></div>
+      <div className={styles.create__card}>
+      <div className={styles.create__image} />
+              <div className={styles.create__attributes}>
+                <div className={styles.create__div}>
+                  <div className={styles.create__gender}>
+                    <h2 className={styles.create__title}>Gender</h2>
+                  </div>
+                  <div className={styles.create__species}>
+                    <h2 className={styles.create__title}>Specie</h2>
+                  </div>
+                </div>
+                <div className={styles.create__origin}>
+                  <h2 className={styles.create__title}>Origin</h2>
+                </div>
+                <div className={styles.create__status}>
+                  <div
+                    className={styles.create__unknown
+                    }
+                  />
+                  <h2 className={styles.create__span}>
+                  </h2>
+                </div>
+              </div>
+      </div>
     </div>
   );
 }
